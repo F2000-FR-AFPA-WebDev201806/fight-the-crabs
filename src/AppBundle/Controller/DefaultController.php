001cde15987f5c6014d39368f8b166fd2019a837
13010-18-05 <13010-18-05@exchange.ad.afpanet>
2018-08-30 14:56:36 +0200

    inscription

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,43 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        return $this->render('@App/Default/index.html.twig', [
         ]);
     }
+
+    /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function inscriptionAction(Request $request) {
+        $oUser = new \AppBundle\Entity\User;
+
+        $oForm = $this->createFormBuilder($oUser)
+                ->add('login', TextType::Class)
+                ->add('password', PasswordType::class)
+                ->getForm();
+
+        $oForm->handleRequest($request);
+        if ($oForm->isSubmitted() && $oForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($oUser);
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('@App/Default/inscription.html.twig', [
+                    'form' => $oForm->createView(),
+        ]);
+    }
+
 }
