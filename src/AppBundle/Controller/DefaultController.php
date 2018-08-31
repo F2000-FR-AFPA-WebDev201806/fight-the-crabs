06d59266b4fa768316adfe35d7b73bbca8f51d5b
13010-18-01 <13010-18-01@exchange.ad.afpanet>
2018-08-31 09:26:26 +0200

    Plateau

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,16 +6,44 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        return $this->render('@App/Default/index.html.twig', [
         ]);
     }
+
+    /**
+     * @Route("/game", name="game")
+     */
+    public function gameAction(Request $request) {
+        //$oRepoGrid = $this->getDoctrine()->getRepository('AppBundle:Grid');
+        //$aGrid = $oGrid->findAll();
+        //dump($aGrid);
+        $aTab1 = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+        ];
+        dump($aTab1);
+        //require_once '/path/to/vendor/autoload.php' ;
+        // replace this example code with whatever you need
+        return $this->render('@App/Default/game.html.twig', [
+                    'grid' => $aTab1,
+        ]);
+    }
+
+    /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function inscriptionAction(Request $request) {
+        // replace this example code with whatever you need
+        return $this->render('@App/Default/game.html.twig', [
+        ]);
+    }
+
 }
